10282ac5fe61e89a1f3ad9ee31832b4b5371aae7
Faruq <faruqabdulsalam@yahoo.com>
2021-07-24 23:00:19 +0100

    Add pick genre js

--- soul/static/pick_genre.js
+++ soul/static/pick_genre.js
@@ -1,35 +1,127 @@
-
+var i=0;
 document.getElementById("Jazz").onclick=function() {removeJazz()};
     function removeJazz() {
+        if (i !== 3) {
         document.getElementById("Jazz1").style.display = "none";
+        i++;
+        }
+        if (i === 3) {
+            document.getElementById("Jazz1").style.display = "none";
+            document.getElementById("AfroPop1").style.display = "none";
+            document.getElementById("AfroBeats1").style.display = "none";
+            document.getElementById("Rythm and Blues1").style.display = "none";
+            document.getElementById("HipHop/Rap1").style.display = "none";
+            document.getElementById("Rock1").style.display = "none";
+            document.getElementById("Dance1").style.display = "none";
+        }
+        
     }
 
 document.getElementById("AfroPop").onclick=function() {removeAfroPop()};
     function removeAfroPop() {
+        if (i !== 3) {
         document.getElementById("AfroPop1").style.display = "none";
+        i++;
+        }
+        if (i === 3) {
+            document.getElementById("Jazz1").style.display = "none";
+            document.getElementById("AfroPop1").style.display = "none";
+            document.getElementById("AfroBeats1").style.display = "none";
+            document.getElementById("Rythm and Blues1").style.display = "none";
+            document.getElementById("HipHop/Rap1").style.display = "none";
+            document.getElementById("Rock1").style.display = "none";
+            document.getElementById("Dance1").style.display = "none";
+        }
+        
     }
 
 document.getElementById("AfroBeats").onclick=function() {removeAfroBeats()};
     function removeAfroBeats() {
+        if (i !== 3) {
         document.getElementById("AfroBeats1").style.display = "none";
+        i++;
+        }
+        if (i === 3) {
+            document.getElementById("Jazz1").style.display = "none";
+            document.getElementById("AfroPop1").style.display = "none";
+            document.getElementById("AfroBeats1").style.display = "none";
+            document.getElementById("Rythm and Blues1").style.display = "none";
+            document.getElementById("HipHop/Rap1").style.display = "none";
+            document.getElementById("Rock1").style.display = "none";
+            document.getElementById("Dance1").style.display = "none";
+        }
+        
     }
 
 document.getElementById("Rythm and Blues").onclick=function() {removeBlues()};
     function removeBlues() {
+        if (i !== 3) {
         document.getElementById("Rythm and Blues1").style.display = "none";
+        i++;
+        }
+        if (i === 3) {
+            document.getElementById("Jazz1").style.display = "none";
+            document.getElementById("AfroPop1").style.display = "none";
+            document.getElementById("AfroBeats1").style.display = "none";
+            document.getElementById("Rythm and Blues1").style.display = "none";
+            document.getElementById("HipHop/Rap1").style.display = "none";
+            document.getElementById("Rock1").style.display = "none";
+            document.getElementById("Dance1").style.display = "none";
+        }
+        
     }
     
 document.getElementById("HipHop/Rap").onclick=function() {removeRap()};
     function removeRap() {
+        if (i !== 3) {
         document.getElementById("HipHop/Rap1").style.display = "none";
+        i++;
+        }
+        if (i === 3) {
+            document.getElementById("Jazz1").style.display = "none";
+            document.getElementById("AfroPop1").style.display = "none";
+            document.getElementById("AfroBeats1").style.display = "none";
+            document.getElementById("Rythm and Blues1").style.display = "none";
+            document.getElementById("HipHop/Rap1").style.display = "none";
+            document.getElementById("Rock1").style.display = "none";
+            document.getElementById("Dance1").style.display = "none";
+        }
+        
     }
 
 document.getElementById("Rock").onclick=function() {removeRock()};
     function removeRock() {
+        if (i !== 3) {
         document.getElementById("Rock1").style.display = "none";
+        i++;
+        }
+        if (i === 3) {
+            document.getElementById("Jazz1").style.display = "none";
+            document.getElementById("AfroPop1").style.display = "none";
+            document.getElementById("AfroBeats1").style.display = "none";
+            document.getElementById("Rythm and Blues1").style.display = "none";
+            document.getElementById("HipHop/Rap1").style.display = "none";
+            document.getElementById("Rock1").style.display = "none";
+            document.getElementById("Dance1").style.display = "none";
+        }
+        
     }
 
 document.getElementById("Dance").onclick=function() {removeDance()};
     function removeDance() {
+        if (i !== 3) {
         document.getElementById("Dance1").style.display = "none";
+        i++;
+        }
+        if (i === 3) {
+            document.getElementById("Jazz1").style.display = "none";
+            document.getElementById("AfroPop1").style.display = "none";
+            document.getElementById("AfroBeats1").style.display = "none";
+            document.getElementById("Rythm and Blues1").style.display = "none";
+            document.getElementById("HipHop/Rap1").style.display = "none";
+            document.getElementById("Rock1").style.display = "none";
+            document.getElementById("Dance1").style.display = "none";
+        }
     }
+
+
