71e9edcc2152ae2a7b290bddc53b78d33c3b20d8
Faruq <faruqabdulsalam@yahoo.com>
2021-07-27 11:00:23 +0100

    Add filtering feature to explore page

--- soul/playlist/views.py
+++ soul/playlist/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from genre.models import UsersGenre
 from .models import Explore, Playlist
 # Create your views here.
 
 def explore_view(request):
     user= request.user
-    explore = Explore.objects.all()
+    user_genre = UsersGenre.objects.filter(user=user) 
+    for genre in user_genre:
+            genre1=genre.user_genres.all()[0]
+            genre2=genre.user_genres.all()[1]
+            genre3=genre.user_genres.all()[2]
+
+    explore = Explore.objects.filter(genre__genres=genre1) | Explore.objects.filter(genre__genres =genre2) | Explore.objects.filter(genre__genres =genre3)  
 
     context = {
         "explore":explore
