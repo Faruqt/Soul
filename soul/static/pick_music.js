3e679ef91aced0c3def216ffa029941d7d2f0b64
Faruq <faruqabdulsalam@yahoo.com>
2021-07-28 17:29:17 +0100

    Add music picking js

---
+++ soul/static/pick_music.js
@@ -0,0 +1,24 @@
+var i=0
+
+function musicAdded() {
+    if (this.style.backgroundColor === "rgb(245, 245, 245)"){
+        this.style.backgroundColor = "rgb(23, 98, 167)"
+        i--
+    }    
+    else {
+        this.style.backgroundColor ="rgb(245, 245, 245)"
+        i++
+    }
+    console.log(i)
+    if(i==1){
+        document.getElementById("playlistAdd").removeAttribute("disabled");
+        document.getElementById("playlistAdd").style.backgroundColor= "#1762A7";
+        document.getElementById("playlistAdd").style.color= "#ffffff"
+    }
+    else if(i==0){
+        document.getElementById("playlistAdd").disabled=true
+        document.getElementById("playlistAdd").style.backgroundColor= "#696a70"
+        document.getElementById("playlistAdd").style.color= "#525357"
+    }
+        
+}
